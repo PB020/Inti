237613e9dfac15b170d38722ec2a521a7bbb41a6
Abenezer Adane <abe05081995@gmail.com>
2021-03-16 15:01:53 -0400

    Implemented proof validation and block addition

--- inti/Blockchain.py
+++ inti/Blockchain.py
@@ -1,4 +1,5 @@
-from inti.Inti import Inti
+import Inti
+import json
 import time
 
 
@@ -17,18 +18,42 @@ class Blockchain:
         """
         Generates the genesis block and appends it to the chain
         """
-        genesis = Inti(0, [], time.time(), "0")
+        genesis = Inti.Inti(0, [], time.time(), "0")
         genesis.hash = genesis.compute_hash()
         self.chain.append(genesis)
 
     @property
-    def previous_block(self) -> Inti:
+    def previous_block(self) -> Inti.Inti:
         """
         Retrieves the most recent block in the chain
         """
         return self.chain[-1]
 
-    def proof_of_work(self, block: Inti) -> str:
+    def proof_validation(self, block: Inti.Inti, block_hash: str) -> bool:
+        """
+        Check if block_hash is valid and satisfies the difficulty criteria
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_block(self, block: Inti.Inti, proof: str) -> bool:
+        """
+        Adds a block onto the chain after verification
+        (proof and previous hash validation)
+        """
+        previous_hash = self.previous_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not Blockchain.proof_validation(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def proof_of_work(self, block: Inti.Inti) -> str:
         """
         Tries different values of the nonce to get a hash that
         satisfies the difficulty criteria
